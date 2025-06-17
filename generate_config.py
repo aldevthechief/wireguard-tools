cc96bcc583fd43893662a43c7b2a7881670c5ade
aldev <ig.sasha71@mail.ru>
2025-06-17 20:54:49 +1000

    init

---
+++ generate_config.py
@@ -0,0 +1,74 @@
+import json
+import random
+import requests
+from datetime import datetime
+from wireguard_tools import WireguardKey
+
+private_key = WireguardKey.generate()
+public_key = private_key.public_key()
+
+curr_time = datetime.now().strftime('%y%m%d%H%M')
+api_link = f'https://api.cloudflareclient.com/v0i{curr_time}/reg'
+
+headers = {
+    'user-agent' : '',
+    'content-type' : 'application/json'
+}
+
+d = {
+    'install_id' : '',
+    'tos' : datetime.now().isoformat()[:-7] + '.000Z',
+    'key': str(public_key),
+    'fcm_token' : '',
+    'type' : 'android',
+    'locale' : 'en_US'
+}
+
+req = requests.post(url=api_link, data=json.dumps(d).replace(' ', ''), headers=headers)
+
+response = json.loads(json.dumps(req.json()))
+
+user_id = response['result']['id']
+token = response['result']['token']
+
+headers = {
+    'user-agent': '',
+    'content-type': 'application/json',
+    'authorization': f'Bearer {token}'
+}
+
+api_link += f'/{user_id}'
+req = requests.patch(url=api_link, data='{"warp_enabled":true}', headers=headers)
+
+response = json.loads(json.dumps(req.json()))
+config = response['result']['config']
+
+peer_key = config['peers'][0]['public_key']
+endpoint = config['peers'][0]['endpoint']['host']
+address_ipv4 = config['interface']['addresses']['v4']
+address_ipv6 = config['interface']['addresses']['v6']
+ports = config['peers'][0]['endpoint']['ports']
+
+s = f'''[Interface]
+PrivateKey = {private_key}
+S1 = 0
+S2 = 0
+Jc = 120
+Jmin = 23
+Jmax = 911
+H1 = 1
+H2 = 2
+H3 = 3
+H4 = 4
+MTU = 1280
+Address = {address_ipv4}, {address_ipv6}
+DNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
+
+[Peer]
+PublicKey = {peer_key}
+AllowedIPs = 0.0.0.0/0, ::/0
+Endpoint = 188.114.99.224:{random.choice(ports)}'''
+
+with open('WARP.conf', 'w') as f:
+    print(s, file=f)
+    
---
+++ ip_bruteforce.py
@@ -0,0 +1,79 @@
+import pathlib
+import socket
+import pywinauto
+from time import sleep
+
+max_connection_retries = 5
+root_dir_path = 'C:\\Users\\admin\\RKN_bypass\\'
+anmezia_wg_path = 'C:\\Program Files\\AmneziaWG\\amneziawg.exe'
+connection_check_host = '74.208.159.18'
+
+    
+def create_config(template : list[str], filenum : int):
+    with open(f'WARP{filenum}.conf', 'w') as f:
+        f.writelines(template)
+        
+    if filenum > 1:
+        pathlib.Path(f'WARP{filenum - 1}.conf').unlink()
+    return
+
+
+if __name__ == '__main__':
+    with open('WARP.conf') as config:
+        config_template = config.readlines()
+
+    endpoint_wrapper = lambda s: f'Endpoint = {s}\n'
+    socket.setdefaulttimeout(2)
+        
+    # ips = open('cf_ips_v4.txt')
+    # networks = list(map(lambda x: ip_network(x.replace('\n', ''), True), ips))
+    # ips.close()
+    
+    app = pywinauto.Application().start(anmezia_wg_path)
+    app = pywinauto.Application().connect(path=anmezia_wg_path)
+    
+    dialog = app.top_window()
+
+    file_num = 0
+    can_connect = False
+    for n1 in range(99, 100):
+        for n2 in range(225, 256):
+            file_num += 1
+            endpoint_addr = f'188.114.{n1}.{n2}:1002'
+            config_template[-1] = endpoint_wrapper(endpoint_addr)
+            create_config(config_template, file_num)
+            
+            dialog['Toolbar'].click()
+
+            dialog = app.top_window()
+            dialog['&Имя файла:Edit'].set_edit_text(f'{root_dir_path}WARP{file_num}.conf')
+            dialog['&ОткрытьButton'].click()
+            
+            dialog = app.top_window()
+            dialog['Button'].click()
+            
+            while not dialog['Button'].is_enabled():
+                sleep(1)
+            
+            attempts = 0
+            while attempts < max_connection_retries:
+                try:
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.connect((connection_check_host, 80))
+                    can_connect = True
+                    break
+                except socket.error:
+                    attempts += 1
+                    sleep(1)
+                
+            dialog['Button'].click()     
+
+            if can_connect:
+                print(endpoint_addr)
+                break
+            else:
+                print('connection failed', endpoint_addr)
+                
+        if can_connect:
+            break
+                    
