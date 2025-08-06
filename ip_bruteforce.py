68623218a56ff371280edcbf1155cc4c1e6af00f
aldev <ig.sasha71@mail.ru>
2025-08-06 17:43:10 +1000

    application connect fix

--- ip_bruteforce.py
+++ ip_bruteforce.py
@@ -28,52 +28,53 @@ if __name__ == '__main__':
     # ips = open('cf_ips_v4.txt')
     # networks = list(map(lambda x: ip_network(x.replace('\n', ''), True), ips))
     # ips.close()
+    cf_ports = [2408, 500, 1701, 4500]
     
     app = pywinauto.Application().start(anmezia_wg_path)
-    app = pywinauto.Application().connect(path=anmezia_wg_path)
+    # app = pywinauto.Application().connect(path=anmezia_wg_path, timeout=5)
+    app = pywinauto.Application().connect(title="AmneziaWG", timeout=5)
     
     dialog = app.top_window()
 
     file_num = 0
     can_connect = False
-    for n1 in range(99, 100):
-        for n2 in range(225, 256):
-            file_num += 1
-            endpoint_addr = f'188.114.{n1}.{n2}:1002'
-            config_template[-1] = endpoint_wrapper(endpoint_addr)
-            create_config(config_template, file_num)
-            
-            dialog['Toolbar'].click()
+    for n1 in cf_ports:
+        file_num += 1
+        endpoint_addr = f'188.114.99.224:{n1}'
+        config_template[-1] = endpoint_wrapper(endpoint_addr)
+        create_config(config_template, file_num)
 
-            dialog = app.top_window()
-            dialog['&Имя файла:Edit'].set_edit_text(f'{root_dir_path}WARP{file_num}.conf')
-            dialog['&ОткрытьButton'].click()
-            
-            dialog = app.top_window()
-            dialog['Button'].click()
-            
-            while not dialog['Button'].is_enabled():
-                sleep(1)
-            
-            attempts = 0
-            while attempts < max_connection_retries:
-                try:
-                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    sock.connect((connection_check_host, 80))
-                    can_connect = True
-                    break
-                except socket.error:
-                    attempts += 1
-                    sleep(1)
-                
-            dialog['Button'].click()     
+        dialog['Toolbar'].click()
 
-            if can_connect:
-                print(endpoint_addr)
+        dialog = app.top_window()
+        dialog['&Имя файла:Edit'].set_edit_text(f'{root_dir_path}WARP{file_num}.conf')
+        dialog['&ОткрытьButton'].click()
+
+        dialog = app.top_window()
+        dialog['Button'].click()
+
+        while not dialog['Button'].is_enabled():
+            sleep(1)
+
+        attempts = 0
+        while attempts < max_connection_retries:
+            try:
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sock.connect((connection_check_host, 80))
+                can_connect = True
                 break
-            else:
-                print('connection failed', endpoint_addr)
-                
+            except socket.error:
+                attempts += 1
+                sleep(1)
+
+        dialog['Button'].click()
+
         if can_connect:
+            print(endpoint_addr)
             break
+        else:
+            print('connection failed', endpoint_addr)
+                
+        # if can_connect:
+        #     break
                     
